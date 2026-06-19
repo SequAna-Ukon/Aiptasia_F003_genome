067f0afae529991d0c2a8824fea9a50671f1ae0f
Abdoallah Sharaf <abdoallah.sharaf@gmail.com>
2026-06-19 20:46:05 +0200

    Create 00.reads_preprocessing.sh for ONT reads
    Added a bash script for ONT reads preprocessing including adapter removal and quality control steps.

--- 00.reads_preprocessing.sh
+++ 00.reads_preprocessing.sh
@@ -1,1 +1,24 @@
-.
+#!/bin/bash
+# ONT reads preprocessing
+
+# ----------------------------------
+# # ENVIRONMENTS & WORKDIRECTORY # #
+workdir="/PATH/TO/READS"
+
+mamba activate reads_env
+mamba install -c bioconda porechop
+mamba install -c bioconda fastqc
+mamba install -c bioconda multiqc
+mamba install -c bioconda NanoPlot
+
+# -------------------------------------
+# # ADAPTER REMOVAL USING PORECHOP # #
+
+porechop --input $workdir/ONT_raw_reads.fastq.gz -o ONT_reads_pc.fastq.gz --discard_middle
+
+# ------------------------------------
+# # QUALITY CONTROL # #
+
+fastqc -t 8 ONT_reads_pc.fastq.gz
+multiqc ONT_reads_pc_fastqc.zip -o multiqc --interactive
+NanoPlot -t 10 --fastq ONT_reads_pc.fastq.gz --plots dot --legacy hex --N50
